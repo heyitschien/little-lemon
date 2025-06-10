fe3765655242a67ef834fd4e6bc9b7e9100628ad
Chien Escalera Duong <heyitschien@gmail.com>
2025-06-10 16:28:55 -0700

    Update footer navigation section
    - Changed 'Door Mat Nav' to 'Site Navigation' for more professional terminology
    - Added missing navigation links (My Reservations, Cart) to match main navigation
    - Ensured consistent navigation structure across the application
    
    Author: Chien Escalera Duong
    Date: 2025-06-10

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -12,12 +12,14 @@ const Footer = () => {
                 </div>
 
                 <div className={`${styles.footerSection} ${styles.navSection}`}>
-                    <h3>Door Mat Nav</h3>
+                    <h3>Site Navigation</h3>
                     <ul>
                         <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/menu">Menu</a></li>
                         <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/my-reservations">My Reservations</a></li>
+                        <li><a href="/cart">Cart</a></li>
                     </ul>
                 </div>
 
